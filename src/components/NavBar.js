54207adc479be688a1ef2e81f41c5a3da0873e4e
= <=>
2019-05-30 13:31:16 +0300

    WIP: responsive navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import MediaQuery from "react-responsive";
 import { updateLoginStatus } from "../utils.js";
 import "../css/Navbar.css";
 
@@ -17,43 +18,59 @@ export default function NavBar({
   purple,
   active
 }) {
+  const [ showMenuMobile, setShowMenuMobile ] = useState(false);
+
   return (
     <div className="top-container" style={purple ? {} : { background: "none" }}>
-      <span
-        className={ active === "journal" ? "active" : "inactive" }
-        onClick={homeHandler}
-      >
-        Journal
-      </span>
-      <span
-        className={ active === "happy" ? "active" : "inactive" }
-        onClick={happyThoughtsHandler}
-      >
-        Happy Thoughts
-      </span>
-      <span
-        className={ active === "habits" ? "active" : "inactive" }
-        onClick={habitsHandler}
-      >
-        Habits
-      </span>
-      <span
-        className={ active === "insights" ? "active" : "inactive" }
-        onClick={insightsHandler}
-      >
-        Insights
-      </span>
-      <span
-        className={ active === "resources" ? "active" : "inactive" }
-        onClick={resourcesHandler}
-      >
-        Resources
-      </span>
-      <span
-        onClick={logout}
-      >
-        Logout
-      </span>
+      <MediaQuery query="(min-width: 768px)">
+        <span
+          className={ "nav-item " + (active === "journal" ? "active" : "inactive") }
+          onClick={homeHandler}
+        >
+          Journal
+        </span>
+        <span
+          className={ "nav-item " + (active === "happy" ? "active" : "inactive") }
+          onClick={happyThoughtsHandler}
+        >
+          Happy Thoughts
+        </span>
+        <span
+          className={ "nav-item " + (active === "habits" ? "active" : "inactive") }
+          onClick={habitsHandler}
+        >
+          Habits
+        </span>
+        <span
+          className={ "nav-item " + (active === "insights" ? "active" : "inactive") }
+          onClick={insightsHandler}
+        >
+          Insights
+        </span>
+        <span
+          className={ "nav-item " + (active === "resources" ? "active" : "inactive") }
+          onClick={resourcesHandler}
+        >
+          Resources
+        </span>
+        <span
+          className="nav-item"
+          onClick={logout}
+        >
+          Logout
+        </span>
+      </MediaQuery>
+      <MediaQuery query="(max-width: 767px)">
+        <img
+          className="menu-icon"
+          src={require("../assets/hamburger.png")}
+          alt="menu button"
+          onClick={() => setShowMenuMobile(!showMenuMobile)}
+        />
+        <span className="website-name">
+          <span style={{ color: "var(--main-green-color)" }}>HappyThoughts</span>.app
+        </span>
+      </MediaQuery>
     </div>
   )
 }
